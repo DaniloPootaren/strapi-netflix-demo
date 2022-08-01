fb1175ab6235359fcd4c0f6b7fa521e5747f9160
Jean Danilo Maxwell Patrick Pootaren <62770019+Daniloxax@users.noreply.github.com>
2022-08-01 10:28:30 +0400

    some changes

--- netflix-angular/src/app/app.module.ts
+++ netflix-angular/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MovieCardComponent } from './components/movie-card/movie-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
